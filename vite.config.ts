27630ae5abd34b23e1246f865cd3a9cacf6d55c3
gonzjv <gonzjv@ya.ru>
2022-08-10 13:50:26 +0300

    build: prettier, @types/node

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,24 @@
-import { defineConfig } from 'vite'
+import { resolve } from 'path'
+import { defineConfig, resolveConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+  plugins: [vue()],
+  build: {
+    lib: {
+      entry: resolve(__dirname, 'lib/main.ts'),
+      name: 'MyLib',
+      fileName: 'my-lib',
+    },
+    rollupOptions: {
+      external: ['vue'],
+      output: {
+        globals: {
+          vue: 'Vue',
+          react: 'Reaqctasdsaasdsa',
+        },
+      },
+    },
+  },
 })
