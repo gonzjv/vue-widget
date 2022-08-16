af3e37ad1d13b68c3654210200fc9faf9aae9db4
gonzjv <gonzjv@ya.ru>
2022-08-16 10:00:52 +0300

    Revert "Revert "build: prettier, @types/node""
    This reverts commit 8fb9b95db5890af50bb8fb0f054b74ef8a778d73.

--- src/App.vue
+++ src/App.vue
@@ -1,36 +1,22 @@
 <script setup lang="ts">
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
+import { HelloWorld } from 'vue-widget';
+
 </script>
 
 <template>
   <div>
     <a href="https://vitejs.dev" target="_blank">
-      <img
-        src="/vite.svg"
-        class="logo"
-        alt="Vite logo"
-      />
+      <img src="/vite.svg" class="logo" alt="Vite logo" />
     </a>
     <a href="https://vuejs.org/" target="_blank">
-      <img
-        src="./assets/vue.svg"
-        class="logo vue"
-        alt="Vue logo"
-      />
+      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
     </a>
   </div>
-  <HelloWorld msg="Vite + Vue" />
-  <!-- <iframe
-    width="680"
-    height="460"
-    src="https://gonzjv.github.io/rsschool-cv/"
-    title="Weather Widget"
-    frameborder="0"
-    allow="accelerometer; clipboard-write; picture-in-picture"
-    allowfullscreen
-  ></iframe> -->
+  <!-- <HelloWorld msg="Vite + Vue" /> -->
+  <HelloWorld  />
 </template>
 
 <style scoped>
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,25 @@
-import { defineConfig } from "vite";
-import vue from "@vitejs/plugin-vue";
+import { resolve } from 'path'
+import { defineConfig, resolveConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-});
+  build: {
+    lib: {
+      entry: resolve(__dirname, 'src/export.ts'),
+      name: 'VueWidget',
+      fileName: (format) =>
+        `vue-widget.${format}.js`,
+    },
+    rollupOptions: {
+      external: ['vue'],
+      output: {
+        globals: {
+          vue: 'Vue',
+          react: 'Reaqctasdsaasdsa',
+        },
+      },
+    },
+  },
+})
