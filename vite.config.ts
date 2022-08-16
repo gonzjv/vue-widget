8fb9b95db5890af50bb8fb0f054b74ef8a778d73
gonzjv <gonzjv@ya.ru>
2022-08-16 09:59:00 +0300

    Revert "build: prettier, @types/node"
    This reverts commit 27630ae5abd34b23e1246f865cd3a9cacf6d55c3.

--- vite.config.ts
+++ vite.config.ts
@@ -1,25 +1,7 @@
-import { resolve } from 'path'
-import { defineConfig, resolveConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  build: {
-    lib: {
-      entry: resolve(__dirname, 'src/export.ts'),
-      name: 'VueWidget',
-      fileName: (format) =>
-        `vue-widget.${format}.js`,
-    },
-    rollupOptions: {
-      external: ['vue'],
-      output: {
-        globals: {
-          vue: 'Vue',
-          react: 'Reaqctasdsaasdsa',
-        },
-      },
-    },
-  },
-})
+});
