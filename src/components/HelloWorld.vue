4c0214528134f4337e67fcd01094d19a45394726
gonzjv <gonzjv@ya.ru>
2022-08-23 12:05:33 +0300

    feat: get weather by city name

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -2,9 +2,19 @@
 import { ref } from 'vue'
 interface IWeatherData {
   name: string
+  sys: {
+    country: string
+  }
+  main: {
+    temp: number
+  }
 }
 const initialWeatherData: IWeatherData = {
-  name: "Minsk"
+  name: 'Minsk',
+  sys: { country: 'BY' },
+  main: {
+    temp: 0,
+  },
 }
 defineProps<{ msg: string }>()
 
@@ -13,48 +23,87 @@ const API_KEY = '2671b0be896edd79fd71f7cdabc7d1dd'
 const city = ref('Minsk')
 let weatherData = ref(initialWeatherData)
 
-
-const getData = async () => {
-
-  const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=35&lon=139&appid=${API_KEY}`)
-  const data = await response.json()
-  console.log("data", data)
-  weatherData.value = data
-  console.log("weatherData", weatherData.value.name)
+const getCoordinates = async (city: string) => {
+  const response = await fetch(
+    `http://api.openweathermap.org/geo/1.0/direct?q=${city}&appid=${API_KEY}`
+  )
+  const [data] = await response.json()
+  // console.log('response', data)
+  return { lat: data.lat, lon: data.lon }
 }
 
-const getCityData = () => {
-  console.log('getCityData for', city.value)
+const getWeatherData = async (city: string) => {
+  const coordinates = await getCoordinates(city)
+  console.log('coordinates', coordinates)
+
+  const response = await fetch(
+    `https://api.openweathermap.org/data/2.5/weather?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${API_KEY}`
+  )
+
+  const data = await response.json()
+  // console.log('data', data)
+  weatherData.value = data
+  console.log(
+    'weatherDataName',
+    weatherData.value.name
+  )
+  console.log('weatherData', weatherData.value)
 }
 </script>
 
 <template>
   <h1>{{ msg }}</h1>
   <p>{{ city }}</p>
-  <form @submit.prevent="getCityData">
-    <input type="text" placeholder="Enter a city..." v-model="city" />
+  <form @submit.prevent="getWeatherData(city)">
+    <input
+      type="text"
+      placeholder="Enter a city..."
+      v-model="city"
+    />
+    <button type="submit">
+      Get Weather Data
+    </button>
   </form>
-  <button @click="getData"> Get Weather Data</button>
-  <div v-if="weatherData.name">{{ weatherData.name }}</div>
+  <div v-if="weatherData.name">
+    <p>{{ weatherData.name }}</p>
+    <p>{{ weatherData.sys.country }}</p>
+    <p>
+      {{ weatherData.main.temp }}
+      <span>Temp</span>
+    </p>
+    <!-- <p>{{ weatherData.sys.country }}</p> -->
+  </div>
   <div class="card">
-    <button type="button" @click="count++">count is {{ count }}</button>
+    <button type="button" @click="count++">
+      count is {{ count }}
+    </button>
     <p>
       Edit
-      <code>components/HelloWorld.vue</code> to test HMR
+      <code>components/HelloWorld.vue</code> to
+      test HMR
     </p>
   </div>
 
   <p>
     Check out
-    <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank">create-vue</a>, the official Vue + Vite
-    starter
+    <a
+      href="https://vuejs.org/guide/quick-start.html#local"
+      target="_blank"
+      >create-vue</a
+    >, the official Vue + Vite starter
   </p>
   <p>
     Install
-    <a href="https://github.com/johnsoncodehk/volar" target="_blank">Volar</a>
+    <a
+      href="https://github.com/johnsoncodehk/volar"
+      target="_blank"
+      >Volar</a
+    >
     in your IDE for a better DX
   </p>
-  <p class="read-the-docs">Click on the Vite and Vue logos to learn more</p>
+  <p class="read-the-docs">
+    Click on the Vite and Vue logos to learn more
+  </p>
 </template>
 
 <style scoped>
