fdb613dede8f6e08cc57d25028d3cd989a41d73f
gonzjv <gonzjv@ya.ru>
2022-08-28 01:32:14 +0300

    feat: edit mode window

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,38 @@
 <script setup lang="ts">
+import { ref } from 'vue';
 import CityItem from './components/CityItem.vue';
+import {
+  Cog8ToothIcon,
+  CheckIcon,
+} from '@heroicons/vue/20/solid';
+
+const editMode = ref(false);
 </script>
 
 <template>
-  <CityItem city="soligorsk" />
+  <main
+    class="relative border-green-300 border-2"
+  >
+    <aside
+      v-if="editMode"
+      class="z-10 absolute top-0 left-0 h-full w-full text-green-600 bg-white"
+    >
+      <button>
+        <CheckIcon
+          @click="editMode = false"
+          class="w-5 absolute top-3 right-3"
+        />
+      </button>
+      edit mode is here!
+    </aside>
+    <button>
+      <Cog8ToothIcon
+        @click="editMode = true"
+        class="w-5 absolute top-3 right-3"
+      />
+    </button>
+    <CityItem city="soligorsk" />
+  </main>
 </template>
 
 <style scoped></style>
--- src/components/CityItem.vue
+++ src/components/CityItem.vue
@@ -25,85 +25,63 @@ onBeforeMount(async () => {
 </script>
 
 <template>
-  <div class="flex flex-col items-center gap-2">
-    <h1>{{ city }}</h1>
-    <!-- <form @submit.prevent="getWeatherData(city)">
-      <input
-        type="text"
-        placeholder="Enter a city..."
-        v-model="city"
-      />
-      <button type="submit">
-        Get Weather Data
-      </button>
-    </form> -->
-    <article
-      v-if="weatherData"
-      class="text-slate-300 flex flex-col gap-3 border-2 border-cyan-200 w-80 p-4"
+  <article
+    v-if="weatherData"
+    class="text-slate-300 flex flex-col gap-3 border-2 border-cyan-200 w-80 p-4"
+  >
+    <header
+      class="flex gap-1 items-center justify-between"
     >
-      <header
-        class="flex gap-1 items-center justify-between"
-      >
-        <div class="flex gap-1 text-xl font-bold">
-          <p>{{ weatherData.name }}</p>
-          <p>,</p>
-          <p>{{ weatherData.sys.country }}</p>
+      <div class="flex gap-1 text-xl font-bold">
+        <p>{{ weatherData.name }}</p>
+        <p>,</p>
+        <p>{{ weatherData.sys.country }}</p>
+      </div>
+      <Cog8ToothIcon class="w-5" />
+    </header>
+    <figure
+      class="flex items-center justify-center"
+    >
+      <img :src="iconUrl" />
+      <figcaption class="font-bold text-3xl">
+        {{ weatherData.main.temp }}
+        <span>&deg;C</span>
+      </figcaption>
+    </figure>
+    <section class="flex gap-1 justify-center">
+      <span>Feels like</span>
+      <span>
+        {{ weatherData.main.feels_like }}&deg;C,
+      </span>
+      <span>
+        {{ weatherData.weather[0].description }}
+      </span>
+    </section>
+    <section class="flex justify-center gap-7">
+      <div class="flex flex-col gap-2">
+        <div class="flex justify-center gap-1">
+          <PaperAirplaneIcon class="w-5" />
+          <p>{{ weatherData.wind.speed }} m/s</p>
         </div>
-        <Cog8ToothIcon class="w-5" />
-      </header>
-      <figure
-        class="flex items-center justify-center"
-      >
-        <img :src="iconUrl" />
-        <figcaption class="font-bold text-3xl">
-          {{ weatherData.main.temp }}
-          <span>&deg;C</span>
-        </figcaption>
-      </figure>
-      <section class="flex gap-1 justify-center">
-        <span>Feels like</span>
-        <span>
-          {{ weatherData.main.feels_like }}&deg;C,
-        </span>
-        <span>
-          {{ weatherData.weather[0].description }}
-        </span>
-      </section>
-      <section class="flex justify-center gap-7">
-        <div class="flex flex-col gap-2">
-          <div class="flex justify-center gap-1">
-            <PaperAirplaneIcon class="w-5" />
-            <p>
-              {{ weatherData.wind.speed }} m/s
-            </p>
-          </div>
-          <div class="flex justify-center gap-1">
-            <ArrowDownTrayIcon class="w-5" />
-            <p>
-              {{ weatherData.main.pressure }} hPa
-            </p>
-          </div>
+        <div class="flex justify-center gap-1">
+          <ArrowDownTrayIcon class="w-5" />
+          <p>
+            {{ weatherData.main.pressure }} hPa
+          </p>
         </div>
-        <div class="flex flex-col gap-2">
-          <div class="flex justify-center gap-1">
-            <BeakerIcon class="w-5" />
-            <p>
-              {{ weatherData.main.humidity }} %
-            </p>
-          </div>
-          <div class="flex justify-center gap-1">
-            <EyeIcon class="w-5" />
-            <p>{{ weatherData.visibility }} m</p>
-          </div>
+      </div>
+      <div class="flex flex-col gap-2">
+        <div class="flex justify-center gap-1">
+          <BeakerIcon class="w-5" />
+          <p>{{ weatherData.main.humidity }} %</p>
         </div>
-      </section>
-    </article>
-    <!-- <div class="card">
-      <button type="button" @click="count++">
-        count is {{ count }}
-      </button>
-    </div> -->
-  </div>
+        <div class="flex justify-center gap-1">
+          <EyeIcon class="w-5" />
+          <p>{{ weatherData.visibility }} m</p>
+        </div>
+      </div>
+    </section>
+  </article>
 </template>
 
 <style scoped>
